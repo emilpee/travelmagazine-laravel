76b86a47defd3479dee26189e32bee25e84bbac5
Åsa Palholmen <asa@palholmen.se>
2019-05-14 09:24:22 +0200

    create Models and Tables migration

---
+++ database/migrations/2019_05_13_135727_create_ads_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->bigIncrements('ad_id');
+            $table->string('name');
+            $table->string('img_url');
+            $table->integer('prio');
+            $table->integer('format');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+}
