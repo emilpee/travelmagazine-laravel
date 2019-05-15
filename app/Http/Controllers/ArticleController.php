e6131129cfe7acec35231e3be27ae30731d6caa5
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-05-15 09:31:24 +0200

    created views for articles and got the data from db out

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class ArticleController extends Controller
 {
@@ -13,7 +14,10 @@ class ArticleController extends Controller
      */
     public function index()
     {
-        //
+        $articles = Article::all();
+        return view('articles.index', [
+            'articles' => $articles
+        ]);
     }
 
     /**
@@ -45,7 +49,10 @@ class ArticleController extends Controller
      */
     public function show($id)
     {
-        //
+        $article = Article::findOrFail($id);
+        return view('/articles/singleArticle', [
+            'article' => $article
+        ]);
     }
 
     /**
