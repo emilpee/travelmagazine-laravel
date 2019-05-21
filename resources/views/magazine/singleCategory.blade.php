5df22fc1c2ad920696b565a5edaf32c2dad1a047
Sandra <otterstensandra@gmail.com>
2019-05-21 15:48:07 +0200

    second page css

--- resources/views/magazine/singleCategory.blade.php
+++ resources/views/magazine/singleCategory.blade.php
@@ -2,12 +2,22 @@
 
 @section('main')
 
-<div>
-    <ul class="category-group">
-        @foreach ($category->articles as $article)
-            <li class="list-group-item"> {{$article}} </li> 
-        @endforeach
-    </ul> 
+<div class="second">
+
+    @foreach ($category->articles as $article)
+        <div class="griditem">
+            <div class="img">
+                <img src="{{$article->img_url}}">
+            </div>
+            <div class="text">
+                <h2> {{$article->title}} </h2> 
+            </div>
+            <div class="text">
+                <h3> {{$article->lead}} </h3>
+            </div>
+        </div>
+    @endforeach
+
 
 </div>
 
