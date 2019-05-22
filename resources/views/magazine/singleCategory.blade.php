0fb907852f062f25eb3771e861fae6e0672bcba7
Åsa Palholmen <asa@palholmen.se>
2019-05-22 12:12:12 +0200

    fixed css on firstpage.

--- resources/views/magazine/singleCategory.blade.php
+++ resources/views/magazine/singleCategory.blade.php
@@ -5,28 +5,23 @@
 <div class="second">
 
     @foreach ($category->articles as $article)
-        <div class="griditem">
-            <div class="img">
-                <img src="{{$article->img_url}}">
-            </div>
-            <div class="text">
-                <h2> {{$article->title}} </h2> 
-            </div>
-            <div class="text">
-                <h3> {{$article->lead}} </h3>
-            </div>
-        </div>
     
-
-
-
-    <article>
-        <a href="{{ route('show', $article->article_id) }}">
-            <img src="{{$article->img_url}}" alt="Archive picture">
-            <h1>{{$article->title}}</h1>
-            <p>{{$article->lead}}</p>
-        </a>
-    </article> 
+    <a href="{{ route('show', $article->article_id) }}">
+        <article class="griditem">
+            
+                <section class="img">
+                    <img src="{{$article->img_url}}" alt="Archive picture">
+                </section>
+                <section class="text">
+                    <h2> {{$article->title}} </h2> 
+                </section>
+                <section class="text">
+                    <h3> {{$article->lead}} </h3>
+                </section>
+            
+        </article>
+    </a>
+   
 
     @endforeach
 </div>
